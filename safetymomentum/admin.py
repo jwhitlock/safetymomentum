218a668097f07a4fc46539dea2b37deeb3682a38
John Whitlock <John-Whitlock@ieee.org>
2015-05-02 21:44:00 -0500

    Improve Moments admin
    - Auto-populate author from request user
    - Auto-populate slug from title

--- safetymomentum/admin.py
+++ safetymomentum/admin.py
@@ -3,6 +3,20 @@
 from django.contrib import admin
 from .models import Image, Keyword, Moment
 
+
+class MomentAdmin(admin.ModelAdmin):
+
+    """Admin for Moment model."""
+
+    prepopulated_fields = {"slug": ("title",)}
+    exclude = ('author',)
+
+    def save_model(self, request, obj, form, change):
+        """Set the user to the request.user."""
+        if not change:
+            obj.author = request.user
+        obj.save()
+
 admin.site.register(Image)
 admin.site.register(Keyword)
-admin.site.register(Moment)
+admin.site.register(Moment, MomentAdmin)
